62961ff28b04468c6448cb5d893fe10e82f61312
luketheneko <lucasneko25@gmail.com>
2024-01-01 01:52:11 -0300

    v1.3.3

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,14 +1,10 @@
-// cookie.js
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Verifique se o usuário já tomou uma decisão sobre os cookies
     if (localStorage.getItem("decisaoCookies")) {
         document.getElementById("cookie-banner").style.display = "none";
     } else {
         document.getElementById("cookie-banner").style.display = "block";
     }
 
-    // Adicione event listeners aos botões "Aceitar" e "Rejeitar"
     document.getElementById("aceitar-cookies").addEventListener("click", function() {
         tomarDecisaoCookies("aceitar");
     });
@@ -21,6 +17,5 @@ document.addEventListener("DOMContentLoaded", function() {
 function tomarDecisaoCookies(decisao) {
     document.getElementById("cookie-banner").style.display = "none";
 
-    // Adicione código aqui para definir um valor no localStorage indicando a decisão do usuário sobre os cookies
     localStorage.setItem("decisaoCookies", decisao);
 }
